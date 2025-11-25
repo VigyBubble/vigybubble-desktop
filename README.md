ebc9def4016728fbf79f87e186735063f6d67366
Mostafa El-Nahas <mostafanahas777@gmail.com>
2025-11-25 15:24:23 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vigybubble-desktop
---
+++ src/main/java/com/effortcure/MainApp.java
@@ -0,0 +1,17 @@
+package com.effortcure;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Vigybubble Desktop");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
