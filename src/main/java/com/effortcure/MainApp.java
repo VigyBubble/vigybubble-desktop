5b7769930ba86d063834549472f8109c29348c80
shahdahmed338 <shahdahmed3579@gmail.com>
2026-03-06 22:54:24 +0200

    edit email-verfication page

--- src/main/java/com/effortcure/MainApp.java
+++ src/main/java/com/effortcure/MainApp.java
@@ -1,13 +1,20 @@
 package com.effortcure;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage)  throws Exception  {
+             Parent root = FXMLLoader.load(getClass().getResource("/fxml/email-verfication-page.fxml"));
+        Scene scene=new Scene(root);
+        stage.setScene(scene);
         stage.setTitle("Vigybubble Desktop");
+        stage.setMaximized(true);
         stage.show();
     }
 
