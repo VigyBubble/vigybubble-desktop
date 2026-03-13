22b512839fce699a2bede0c80f41a9a445947804
asmaa mamdouh <am3013522@gmail.com>
2026-03-13 23:04:42 +0200

    edit register and login pages

--- src/main/java/com/effortcure/MainApp.java
+++ src/main/java/com/effortcure/MainApp.java
@@ -6,20 +6,25 @@ import com.effortcure.navigator.SceneManager;
 import com.effortcure.util.ScreenDimensionsUtil;
 
 import javafx.application.Application;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+    Font.loadFont(getClass().getResourceAsStream("/fonts/K2D-Regular.ttf"), 12);
+    Font.loadFont(getClass().getResourceAsStream("/fonts/K2D-SemiBold.ttf"), 12);
+    Font.loadFont(getClass().getResourceAsStream("/fonts/K2D-Bold.ttf"), 12);
+
         double[] screenDimensions = ScreenDimensionsUtil.getPlatformFullScreenViewDimensions();
         System.setProperty("os.screen.width", String.valueOf(screenDimensions[0]));
         System.setProperty("os.screen.hight", String.valueOf(screenDimensions[1]));
         SceneManager.setStage(stage);
-        SceneManager.switchScene("/fxml/register-page.fxml");
+        SceneManager.switchScene("/fxml/login-page.fxml");
     }
-
     public static void main(String[] args) {
         launch(args);
     }
 }
+
