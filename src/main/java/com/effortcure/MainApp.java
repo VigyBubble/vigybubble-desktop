640aac0a6dd6dfb051c6ea69bd2a868a52908507
asmaa mamdouh <am3013522@gmail.com>
2026-03-15 23:35:24 +0200

    edit login.fxml

--- src/main/java/com/effortcure/MainApp.java
+++ src/main/java/com/effortcure/MainApp.java
@@ -16,11 +16,7 @@ public class MainApp extends Application {
         System.setProperty("os.screen.width", String.valueOf(screenDimensions[0]));
         System.setProperty("os.screen.hight", String.valueOf(screenDimensions[1]));
         SceneManager.setStage(stage);
-<<<<<<< HEAD
-        SceneManager.switchScene("/fxml/login-page.fxml");
-=======
-        SceneManager.switchScene("/fxml/email-verfication-page.fxml");
->>>>>>> origin/main
+        SceneManager.switchScene("/fxml/new-password-page.fxml");
     }
 
     public static void main(String[] args) {
