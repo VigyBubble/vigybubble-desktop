2750cf71b0eb72355bc7f96116c53300cca06cf9
Mostafa El-Nahas <mostafanahas777@gmail.com>
2026-04-04 17:34:56 +0200

    Clean incorrect code

--- src/main/java/com/effortcure/controller/LoginPageController.java
+++ src/main/java/com/effortcure/controller/LoginPageController.java
@@ -1,16 +1,5 @@
 package com.effortcure.controller;
 
-import com.effortcure.dto.response.ApiResponse;
-import com.effortcure.service.implementation.AuthService;
-import com.effortcure.service.interfaces.AuthServiceInterface;
-import com.effortcure.util.BooleanWrapperUtil;
-import com.effortcure.util.ControllersUtil;
-import com.effortcure.util.JsonUtil;
-import com.effortcure.util.SceneManager;
-import com.effortcure.util.ViewUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -20,7 +9,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class LoginPageController {
@@ -96,101 +84,9 @@ public class LoginPageController {
     @FXML
     private Button buttonLogin;
 
-    private AuthServiceInterface authService = new AuthService();
-
-
-    private boolean isValidEmail;
-    private boolean isValidPassword;
-    private StringBuilder password = new StringBuilder();
-    private BooleanWrapperUtil maskPasswordFeild = new BooleanWrapperUtil();
-
-
-
     @FXML
     private void initialize() {
-        ViewUtil.initiateResponsiveView(this);
-        validateLoginData();
-        ControllersUtil.onMouseSelection(new TextField[] { passwordField });
-        ControllersUtil.disableTextFeildPasting(new TextField[] { passwordField });
-        ControllersUtil.getTypedTextOnKeyTypedOrDelete(passwordField, (typedText) -> {
-            password = typedText;
-        });
-        maskPasswordFeild.setBool(true);
-        ViewUtil.maskTextFeildContent(passwordField, maskPasswordFeild);
+
     }
 
-      @FXML
-    private void login(){
-   
-    }
-
-    @FXML
-    private void register() {
-        SceneManager.switchScene("/fxml/new-password-page.fxml");
-    }
-
-    @FXML
-    private void hideAndShowPassword() {
-        if (maskPasswordFeild.getBool()) {
-            locklIcon.setImage(new Image(getClass().getResource("/images/open-eye.png").toExternalForm()));
-            maskPasswordFeild.setBool(false);
-            int cursorPositionBeforeSettingFeild = passwordField.getCaretPosition();
-            passwordField.setText(password.toString());
-            passwordField.positionCaret(cursorPositionBeforeSettingFeild);
-
-        } else {
-            locklIcon.setImage(new Image(getClass().getResource("/images/locked-eye.png").toExternalForm()));
-            maskPasswordFeild.setBool(true);
-            int cursorPositionBeforeSettingFeild = passwordField.getCaretPosition();
-            passwordField.setText(passwordField.getText() + " ");
-            passwordField.positionCaret(cursorPositionBeforeSettingFeild);
-        }
-    }
-
-    private void validateLoginData() {
-        textFieldEmail.textProperty().addListener((obs, oldValue, newValue) -> {
-            if (newValue.isBlank()) {
-                emailErrorMsg.setText("email is required *");
-                ViewUtil.showHiddenErrorMessages(new Label[] { emailErrorMsg });
-                textFieldEmail.getStyleClass().add("error-field");
-                isValidEmail = false;
-            } else if (!newValue.matches("^[a-z0-9]+(?:[._][a-z0-9]+)*(?:\\+[a-z0-9]+)?@gmail\\.com$")) {
-                emailErrorMsg.setText("must match a-z, 0-9, [._%+-] @gmail.com *");
-                ViewUtil.showHiddenErrorMessages(new Label[] { emailErrorMsg });
-                textFieldEmail.getStyleClass().add("error-field");
-                isValidEmail = false;
-            } else {
-                ViewUtil.hideErrorMessages(new Label[] { emailErrorMsg });
-                textFieldEmail.getStyleClass().removeAll("error-field");
-                isValidEmail = true;
-            }
-        });
-        passwordField.textProperty().addListener((obs, oldValue, newValue) -> {
-            if (newValue.isBlank()) {
-                if (!passwordErrorMsg.getText().equals("can't paste here *"))
-                    passwordErrorMsg.setText("password is required *");
-                ViewUtil.showHiddenErrorMessages(new Label[] { passwordErrorMsg });
-                passwordField.getStyleClass().add("error-field");
-                password.setLength(0);
-                isValidPassword = false;
-            } else if (!password.toString().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{8,25}$")) {
-                passwordErrorMsg.setText("must have A-Z, a-z, 0-9, special characters *");
-                if (password.toString().length() < 8 || password.toString().length() > 25)
-                    passwordErrorMsg.setText("must be between 8-25 characters *");
-                ViewUtil.showHiddenErrorMessages(new Label[] { passwordErrorMsg });
-                passwordField.getStyleClass().add("error-field");
-                isValidPassword = false;
-            } else {
-                ViewUtil.hideErrorMessages(new Label[] { passwordErrorMsg });
-                passwordField.getStyleClass().removeAll("error-field");
-                isValidPassword = true;
-            }
-        });
-    }
-    private boolean isValidLoginData() {
-        return isValidEmail && isValidPassword ;
-    }
 }
-
-
-
--- src/main/java/com/effortcure/controller/NewPasswordPageController.java
+++ src/main/java/com/effortcure/controller/NewPasswordPageController.java
@@ -1,17 +1,10 @@
 package com.effortcure.controller;
 
-import com.effortcure.service.implementation.AuthService;
-import com.effortcure.service.interfaces.AuthServiceInterface;
-import com.effortcure.util.BooleanWrapperUtil;
-import com.effortcure.util.ControllersUtil;
-import com.effortcure.util.ViewUtil;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -71,117 +64,9 @@ public class NewPasswordPageController {
     @FXML
     private Button saveButton;
 
-    private AuthServiceInterface authService = new AuthService();
-
-    private boolean isValidPassword;
-    private boolean isValidConfirmPassword;
-    private StringBuilder password = new StringBuilder();
-    private StringBuilder confirmPassword = new StringBuilder();
-    private BooleanWrapperUtil maskPasswordFeild = new BooleanWrapperUtil();
-    private BooleanWrapperUtil maskConfirmPasswordFeild = new BooleanWrapperUtil();
-
     @FXML
     private void initialize() {
-        ViewUtil.initiateResponsiveView(this);
-        validateNewPasswordData();
 
-        ControllersUtil.onMouseSelection(new TextField[] { passwordField, confirmPasswordField });
-        ControllersUtil.disableTextFeildPasting(new TextField[] { passwordField, confirmPasswordField });
-        ControllersUtil.getTypedTextOnKeyTypedOrDelete(passwordField, (typedText) -> {
-            password = typedText;
-        });
-        ControllersUtil.getTypedTextOnKeyTypedOrDelete(confirmPasswordField, (typedText) -> {
-            confirmPassword = typedText;
-        });
-        maskPasswordFeild.setBool(true);
-        maskConfirmPasswordFeild.setBool(true);
-        ViewUtil.maskTextFeildContent(passwordField, maskPasswordFeild);
-        ViewUtil.maskTextFeildContent(confirmPasswordField, maskConfirmPasswordFeild);
-    }
-    @FXML
-    private void Save() {
-
-    }
-    
-    @FXML
-    private void hideAndShowPassword() {
-        if (maskPasswordFeild.getBool()) {
-            passwordLock.setImage(new Image(getClass().getResource("/images/open-eye.png").toExternalForm()));
-            maskPasswordFeild.setBool(false);
-            int cursorPositionBeforeSettingFeild = passwordField.getCaretPosition();
-            passwordField.setText(password.toString());
-            passwordField.positionCaret(cursorPositionBeforeSettingFeild);
-
-        } else {
-            passwordLock.setImage(new Image(getClass().getResource("/images/locked-eye.png").toExternalForm()));
-            maskPasswordFeild.setBool(true);
-            int cursorPositionBeforeSettingFeild = passwordField.getCaretPosition();
-            passwordField.setText(passwordField.getText() + " ");
-            passwordField.positionCaret(cursorPositionBeforeSettingFeild);
-        }
-    }
-
-    @FXML
-    private void hideAndShowConfirmPassword() {
-        if (maskConfirmPasswordFeild.getBool()) {
-            confirmPasswordLock.setImage(new Image(getClass().getResource("/images/open-eye.png").toExternalForm()));
-            maskConfirmPasswordFeild.setBool(false);
-            int cursorPositionBeforeSettingFeild = confirmPasswordField.getCaretPosition();
-            confirmPasswordField.setText(confirmPassword.toString());
-            confirmPasswordField.positionCaret(cursorPositionBeforeSettingFeild);
-        } else {
-            confirmPasswordLock.setImage(new Image(getClass().getResource("/images/locked-eye.png").toExternalForm()));
-            maskConfirmPasswordFeild.setBool(true);
-            int cursorPositionBeforeSettingFeild = confirmPasswordField.getCaretPosition();
-            confirmPasswordField.setText(confirmPasswordField.getText() + " ");
-            confirmPasswordField.positionCaret(cursorPositionBeforeSettingFeild);
-        }
-    }
-    private void validateNewPasswordData() {
-        passwordField.textProperty().addListener((obs, oldValue, newValue) -> {
-            if (newValue.isBlank()) {
-                if (!passwordErrorMsg.getText().equals("can't paste here *"))
-                    passwordErrorMsg.setText("password is required *");
-                ViewUtil.showHiddenErrorMessages(new Label[] { passwordErrorMsg });
-                passwordField.getStyleClass().add("error-field");
-                password.setLength(0);
-                isValidPassword = false;
-            } else if (!password.toString().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{8,25}$")) {
-                passwordErrorMsg.setText("must have A-Z, a-z, 0-9, special characters *");
-                if (password.toString().length() < 8 || password.toString().length() > 25)
-                    passwordErrorMsg.setText("must be between 8-25 characters *");
-                ViewUtil.showHiddenErrorMessages(new Label[] { passwordErrorMsg });
-                passwordField.getStyleClass().add("error-field");
-                isValidPassword = false;
-            } else {
-                ViewUtil.hideErrorMessages(new Label[] { passwordErrorMsg });
-                passwordField.getStyleClass().removeAll("error-field");
-                isValidPassword = true;
-            }
-        });
-        confirmPasswordField.textProperty().addListener((obs, oldValue, newValue) -> {
-            if (newValue.isBlank()) {
-                if (!confirmpasswordErrorMsg.getText().equals("can't paste here *"))
-                    confirmpasswordErrorMsg.setText("password is required *");
-                ViewUtil.showHiddenErrorMessages(new Label[] { confirmpasswordErrorMsg });
-                confirmPasswordField.getStyleClass().add("error-field");
-                confirmPassword.setLength(0);
-                isValidConfirmPassword = false;
-            } else if (!password.toString().equals(confirmPassword.toString())) {
-                confirmpasswordErrorMsg.setText("password mismatch*");
-                ViewUtil.showHiddenErrorMessages(new Label[] { confirmpasswordErrorMsg });
-                confirmPasswordField.getStyleClass().add("error-field");
-                isValidConfirmPassword = false;
-            } else {
-                ViewUtil.hideErrorMessages(new Label[] { confirmpasswordErrorMsg });
-                confirmPasswordField.getStyleClass().removeAll("error-field");
-                isValidConfirmPassword = true;
-            }
-        });
-    }
-
-    private boolean isValidRegisterData() {
-        return  isValidPassword && isValidConfirmPassword;
     }
 
 }
