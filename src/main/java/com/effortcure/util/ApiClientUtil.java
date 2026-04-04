8c8ba3a7d3c5cb03ae8fe375591f1438a8e89678
Mostafa El-Nahas <mostafanahas777@gmail.com>
2026-04-04 19:06:52 +0200

    adding get, patch, put, delete requests to the ApiClientUtil

--- src/main/java/com/effortcure/util/ApiClientUtil.java
+++ src/main/java/com/effortcure/util/ApiClientUtil.java
@@ -6,26 +6,67 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ApiClientUtil {
-        private static final HttpClient client = HttpClient.newHttpClient();
+    private static final HttpClient client = HttpClient.newHttpClient();
 
-        public static HttpResponse<String> post(String url, String json, String accessToken, String refreshToken)
-                        throws Exception {
-                HttpRequest.Builder builder = HttpRequest.newBuilder()
-                                .uri(new URI(url))
-                                .header("Content-Type", "application/json")
-                                .header("Accept", "application/json")
-                                .headers("User-Agent",
-                                                "JavaFxApp: OS-" + System.getProperty("os.name") + " OS-VERSION-"
-                                                                + System.getProperty("os.version") + " ARCH-"
-                                                                + System.getProperty("os.arch"));
-                if (accessToken != null) {
-                        builder.header("Authorization", "Bearer " + accessToken);
-                }
-                if (refreshToken != null) {
-                        builder.header("Cookie", "refresh-token=" + refreshToken);
-                }
+    public static HttpResponse<String> get(String url, String json, String accessToken, String refreshToken)
+            throws Exception {
+        HttpRequest.Builder builder = commonRequestPart(url, accessToken, refreshToken);
+        HttpRequest request = builder.GET().build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
 
-                HttpRequest request = builder.POST(HttpRequest.BodyPublishers.ofString(json)).build();
-                return client.send(request, HttpResponse.BodyHandlers.ofString());
+    public static HttpResponse<String> post(String url, String json, String accessToken, String refreshToken)
+            throws Exception {
+        HttpRequest.Builder builder = commonRequestPart(url, accessToken, refreshToken);
+        HttpRequest request = builder
+                .POST((json != null) ? HttpRequest.BodyPublishers.ofString(json) : HttpRequest.BodyPublishers.noBody())
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static HttpResponse<String> put(String url, String json, String accessToken, String refreshToken)
+            throws Exception {
+        HttpRequest.Builder builder = commonRequestPart(url, accessToken, refreshToken);
+        HttpRequest request = builder
+                .PUT((json != null) ? HttpRequest.BodyPublishers.ofString(json) : HttpRequest.BodyPublishers.noBody())
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static HttpResponse<String> patch(String url, String json, String accessToken, String refreshToken)
+            throws Exception {
+        HttpRequest.Builder builder = commonRequestPart(url, accessToken, refreshToken);
+        HttpRequest request = builder
+                .method("PATCH",
+                        (json != null) ? HttpRequest.BodyPublishers.ofString(json)
+                                : HttpRequest.BodyPublishers.noBody())
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static HttpResponse<String> delete(String url, String json, String accessToken, String refreshToken)
+            throws Exception {
+        HttpRequest.Builder builder = commonRequestPart(url, accessToken, refreshToken);
+        HttpRequest request = builder.DELETE().build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    private static HttpRequest.Builder commonRequestPart(String url, String accessToken, String refreshToken)
+            throws Exception {
+        HttpRequest.Builder builder = HttpRequest.newBuilder()
+                .uri(new URI(url))
+                .header("Content-Type", "application/json")
+                .header("User-Agent",
+                        "JavaFxApp/1.0 (" +
+                                System.getProperty("os.name") + "; " +
+                                System.getProperty("os.version") + "; " +
+                                System.getProperty("os.arch") + ")");
+        if (accessToken != null) {
+            builder.header("Authorization", "Bearer " + accessToken);
         }
+        if (refreshToken != null) {
+            builder.header("Cookie", "refresh-token=" + refreshToken);
+        }
+        return builder;
+    }
 }
