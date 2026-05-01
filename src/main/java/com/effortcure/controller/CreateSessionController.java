12544371a9baa01f079408e7a774e8abb39500d8
shahdahmed338 <shahdahmed3579@gmail.com>
2026-05-01 12:17:38 +0300

    modify createSession page

--- src/main/java/com/effortcure/controller/CreateSessionController.java
+++ src/main/java/com/effortcure/controller/CreateSessionController.java
@@ -1,7 +1,10 @@
 package com.effortcure.controller;
 
+import com.effortcure.navigator.ContentManager;
 import com.effortcure.util.ViewUtil;
+import javafx.util.Duration;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
@@ -17,7 +20,8 @@ public class CreateSessionController {
 
   @FXML
   private AnchorPane root;
-
+  @FXML
+  private ImageView back;
   @FXML
   private ImageView bubble1;
   @FXML
@@ -51,6 +55,25 @@ public class CreateSessionController {
   private void initialize() {
 
     ViewUtil.initiateResponsiveView(this);
+        animateBubble(bubble1, 20, 3);
+        animateBubble(bubble2, 15, 4);
+        animateBubble(bubble3, 25, 5);
+        animateBubble(bubble4, 18, 3.5);
   }
+    @FXML
+    private void backToInspectBubble() {
+        ContentManager.setAnchorPane(root);
+        ContentManager.switchContent("/fxml/inspect-bubble.fxml");
+    }
+       private void animateBubble(ImageView bubble, double moveY, double duration) {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setNode(bubble);
+        transition.setDuration(Duration.seconds(duration));
+        transition.setByY(-moveY);
+        transition.setByX(10);
+        transition.setAutoReverse(true);
+        transition.setCycleCount(TranslateTransition.INDEFINITE);
+        transition.play();
+    }
 
 }
--- src/main/java/com/effortcure/controller/InspectBubbleController.java
+++ src/main/java/com/effortcure/controller/InspectBubbleController.java
@@ -19,7 +19,6 @@ import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
