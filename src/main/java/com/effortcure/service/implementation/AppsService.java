f1097f3700470c791c0fefaa5baf65f421a2b441
Mostafa El-Nahas <mostafanahas777@gmail.com>
2026-05-03 22:45:32 +0300

    remove warnnings

--- src/main/java/com/effortcure/service/implementation/AppsService.java
+++ src/main/java/com/effortcure/service/implementation/AppsService.java
@@ -10,16 +10,16 @@ import com.fasterxml.jackson.core.type.TypeReference;
 
 public class AppsService implements AppsServiceInterface {
 
-        @Override
-        public List<AppResponseDTO> getApps() throws Exception {
-                String host = System.getenv("AGENT_HOST");
-                String port = System.getenv("AGENT_PORT");
-                SocketClient socketClient = new SocketClient();
-                socketClient.setHost(host != null ? host : "127.0.0.1");
-                socketClient.setPort((port != null && !port.isBlank()) ? Integer.parseInt(port) : 8080);
-                String json = socketClient.send("get_apps");
-                return JsonUtil.fromJson(json, new TypeReference<List<AppResponseDTO>>() {
-                });
-        }
+    @Override
+    public List<AppResponseDTO> getApps() throws Exception {
+        String host = System.getenv("AGENT_HOST");
+        String port = System.getenv("AGENT_PORT");
+        SocketClient socketClient = new SocketClient();
+        socketClient.setHost(host != null ? host : "127.0.0.1");
+        socketClient.setPort((port != null && !port.isBlank()) ? Integer.parseInt(port) : 8080);
+        String json = socketClient.send("get_apps");
+        return JsonUtil.fromJson(json, new TypeReference<List<AppResponseDTO>>() {
+        });
+    }
 
 }
