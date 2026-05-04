3f38dc00887c90d345a99625a397e453fcbf23af
Mostafa El-Nahas <mostafanahas777@gmail.com>
2026-05-05 01:15:49 +0300

    view performance metrics

--- src/main/java/com/effortcure/controller/InspectSessionsController.java
+++ src/main/java/com/effortcure/controller/InspectSessionsController.java
@@ -156,6 +156,7 @@ public class InspectSessionsController {
         if (sessionUuid != null) {
             loadSessionData();
         }
+        viewPerformanceMetrics();
         getNotifications();
         pauseImage = new Image(getClass().getResource("/images/pause-icon.png").toExternalForm());
         playImage = new Image(getClass().getResource("/images/play-button.png").toExternalForm());
@@ -219,11 +220,11 @@ public class InspectSessionsController {
     private void getNotifications() throws Exception {
         ApiResponse<Set<LoggedNotificationResponseDTO>> response = sessionsServiceInterface
                 .getLoggedNotifications(sessionUuid);
-       
+
         if (response != null) {
             Set<LoggedNotificationResponseDTO> notifications = response.getData() != null ? response.getData()
                     : new HashSet<>();
-          
+
             for (LoggedNotificationResponseDTO dto : notifications) {
 
                 Pane pane = FXMLLoader.load(
@@ -294,6 +295,18 @@ public class InspectSessionsController {
                 contentPane.setStyle(" -fx-effect:innershadow(gaussian, #7022B9, 10, 0.2, 0, 0)");
                 break;
         }
+    }
+
+    private void viewPerformanceMetrics() throws Exception {
+        ApiResponse<PerformanceMetricsResponseDTO> response = sessionsServiceInterface
+                .getPerformanceMetrics(sessionUuid);
+        if (response != null) {
+            for (int i = 0; i < 20; i++) {
+                ContentManager.setPane((Pane) sessionsGridPane.lookup("#pane" + i));
+                ContentManager.switchPaneContent("/fxml/progress-circle-card.fxml");
+            }
+        }
+
     }
 
     @FXML
--- src/main/java/com/effortcure/navigator/ContentManager.java
+++ src/main/java/com/effortcure/navigator/ContentManager.java
@@ -5,9 +5,15 @@ import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class ContentManager {
     private static AnchorPane anchorPane;
+    private static Pane pane;
+
+    public static void setPane(Pane pane) {
+        ContentManager.pane = pane;
+    }
 
     public static void setAnchorPane(AnchorPane anchorPane) {
         ContentManager.anchorPane = anchorPane;
@@ -22,11 +28,20 @@ public class ContentManager {
         }
     }
 
-    //ده لو احتاجت استخدم controller
+    // ده لو احتاجت استخدم controller
     public static <T> T switchContentWithController(String fxml) throws IOException {
         FXMLLoader loader = new FXMLLoader(ContentManager.class.getResource(fxml));
         Parent content = loader.load();
         anchorPane.getChildren().setAll(content);
         return loader.getController();
+    }
+
+    public static void switchPaneContent(String fxml) {
+        try {
+            Parent view = FXMLLoader.load(ContentManager.class.getResource(fxml));
+            pane.getChildren().setAll(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
